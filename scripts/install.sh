a9227c494ff8167512469d4dbeb89f9af9a86048
Justin Woo <moomoowoo@gmail.com>
2014-11-29 10:11:08 -0600

    Separate out my package lists into separate files
    Better organization, easier to look for and find

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,9 @@
 ln -nsf `pwd`/dotfiles/vimrc ~/.vimrc
 ln -nsf `pwd`/dotfiles/gvimrc ~/.gvimrc
 ln -nsf `pwd`/dotfiles/vundlerc ~/.vundlerc
+ln -nsf `pwd`/dotfiles/vundlerc.utils ~/.vundlerc.utils
+ln -nsf `pwd`/dotfiles/vundlerc.langs ~/.vundlerc.langs
+ln -nsf `pwd`/dotfiles/vundlerc.colors ~/.vundlerc.colors
 ln -nsf `pwd`/vim ~/.vim
 
 echo 'symlinks made. check scripts/install.sh for details'
